bc916b5c64e5b7dd4e0c50cf895d2112b9c1a480
Bishal Subedi <theb8821@gmail.com>
2025-07-15 23:14:35 -0400

    Update Footer Date

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css'
 function Footer() {
   return (
     <section id='footer' className={styles.container}>
-        <p>&copy;2024 Bishal Subedi <br/> All right reserved.</p>
+        <p>&copy;2025 Bishal Subedi <br/> All right reserved.</p>
     </section>
   )
 }
