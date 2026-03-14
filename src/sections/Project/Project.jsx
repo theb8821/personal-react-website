b03aa8d4e4c9328e572779617b8d1af775344434
theb8821 <theb8821@gmail.com>
2026-03-14 11:51:33 -0500

    Update Project items

--- src/sections/Project/Project.jsx
+++ src/sections/Project/Project.jsx
@@ -1,6 +1,6 @@
 import styles from './ProjectStyles.module.css';
 import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
+import extension from '../../assets/extension.svg';
 import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
@@ -11,7 +11,7 @@ function Projects() {
 			<h1 className="sectionTitle">Projects</h1>
 			<div className={styles.projectsContainer}>
 				<ProjectCard src={viberr} link="https://github.com/theb8821/FlappyBird" name="FlappyBird" desc="Pygame Recreation" />
-				<ProjectCard src={freshBurger} link="https://github.com/theb8821/personal-website" name="Personal Website" desc="HTML, CSS and BootStrap" />
+				<ProjectCard src={extension} link="https://github.com/theb8821/Youtube-Auto-Scroller" name="Chrome Extension" desc="Auto Scroll YouTube Shorts" />
 				<ProjectCard src={hipsster} link="https://github.com/theb8821/PingPong" name="PingPong" desc="Python Reacreation" />
 				<ProjectCard src={fitLift} link="https://github.com/theb8821/Bill-Management-System-C" name="Billy" desc="Billing System in C" />
 			</div>
