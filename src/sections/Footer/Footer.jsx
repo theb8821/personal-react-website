d08eae72aabd99542ec4088647f90d3a2be75827
Bishal Subedi <theb@Bishals-MacBook-Air.local>
2026-03-14 11:39:59 -0500

    Update Date Script to Show Current Year

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css'
 function Footer() {
   return (
     <section id='footer' className={styles.container}>
-        <p>&copy;2025 Bishal Subedi <br/> All right reserved.</p>
+        <p>&copy;{new Date().getFullYear()} Bishal Subedi <br/> All right reserved.</p>
     </section>
   )
 }
